7564dcac69b7901706413be7ef63b0a536d60627
Parth Ray <parthray16@gmail.com>
2021-12-05 15:34:09 -0800

    py funcs to backend

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, json
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
@@ -17,7 +17,13 @@ def fill_calendar():
 def extract_conference():
     if request.method == 'POST':
         emailText = request.get_json()['text']
-       # conference = python_fun(emailText)
-       # resp = jsonify(conference), 201
+        extraction = { 'name': '', 'acronym': '', 'submission': '', 'notification': '', 'conference': '', 'location': '' }
+        #extraction['name'] = pythonfunction(emailText)
+        #extraction['acronym'] = pythonfunction(emailText)
+        #extraction['submission'] = pythonfunction(emailText)
+        #extraction['notification'] = pythonfunction(emailText)
+        #extraction['conference'] = pythonfunction(emailText)
+        #extraction['location'] = pythonfunction(emailText)
+        #resp = jsonify(extraction), 200
         resp = jsonify({ 'name': '7th International Conference on Signal Processing and Integrated Networks', 'acronym': '', 'submission': '11/11/2019', 'notification': '12/9/2019', 'conference': '2/27/2020 - 3/1/2020', 'location': 'Delhi/NCR, India' }), 200
         return resp
